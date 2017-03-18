0d0a7fea96ea14b7ded8af8012c94011c6ff99c6
Muhmd <Muhmd>
2017-03-18 17:45:56 +0200

    first commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 const express = require('express');
-const port = process.env.PORT || 3000 ; 
+const port = process.env.PORT || 3000 ;
 const publicPath = path.join(__dirname , '../public')
 
 
 var app = express();
+var server = http.createServer(app)
+var io=socketIO(server)
 
 app.use(express.static(publicPath));
 
-app.listen(port);
+server.listen(port);
