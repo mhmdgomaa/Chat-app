f5a40f7a1588df9d14d359d5fd66226792e9654e
Muhmd <Muhmd>
2017-03-19 15:19:51 +0200

    3 commit

--- public/js/index.js
+++ public/js/index.js
@@ -9,4 +9,22 @@ socket.on('connect' ,function (){
     console.log('the server disconnect');})
 
   socket.on('newMessage',function (message) {
-      console.log('newMessage', message);})
+      console.log('newMessage', message);
+      var li = jQuery('<li></li>');
+      li.text(`${message.from}:  ${message.text} .`);
+      jQuery('#messages').append(li);
+    })
+
+    // socket.emit('creatMessage', {
+    //   from : 'om omar',
+    //   text : 'hafshakhk'
+    // } , (data) => { console.log('Got it ' , data);})
+
+jQuery('#Ahly-message').on('submit', (e)=>{
+  e.preventDefault();
+
+  socket.emit('creatMessage' , {
+      from : 'User' ,
+    text : jQuery('[name=Massage]').val()
+  }, ()=> {})
+})
--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const path = require('path');
 const socketIO = require('socket.io');
 const http = require('http');
 const express = require('express');
+
+const { generateMessage } = require('./utils/message')
+
 const port = process.env.PORT || 3000 ;
 const publicPath = path.join(__dirname , '../public')
 
@@ -13,21 +16,19 @@ var io=socketIO(server)
 io.on('connection', (socket)=>{
   console.log('New user is connected');
 
-socket.emit('newMessage', {
-  from : 'Admin', text: 'Welcome to chat room' ,createdAt : new Date().getTime()
-});
+  socket.emit('newMessage',
+      generateMessage('Admin' , 'Welcome to the chat room')
+);
 
-socket.broadcast.emit('newMessage', {
-  from :'Admin', text : 'New user has joined',createdAt : new Date().getTime()
-});
+  socket.broadcast.emit('newMessage',
+      generateMessage('Admin' , 'new user has joined')
+);
 
-  socket.on('creatMessage', function (message) {
+  socket.on('creatMessage', function (message , callback) {
     console.log('creatMessage', message );
-    socket.broadcast.emit('newMessage',{
-      text: message.text ,
-      from: message.from ,
-      createdAt : new Date().getTime()
-    } );
+    io.emit('newMessage',generateMessage(message.from , message.text)
+     );
+     callback( 'from the server' );
   });
 
   socket.on('disconnect', ()=>{
